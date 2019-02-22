147dcfd4f61ae85da14f55c82fb53fc4a1766136
Yuwei Zhou <yuwzho@microsoft.com>
2019-02-22 17:07:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,1 @@
-var http = require('http');
-
-var handleRequest = function(request, response) {
-  console.log('Received request for URL: ' + request.url);
-  response.writeHead(200);
-  response.end('Hello World!!!');
-};
-var www = http.createServer(handleRequest);
-www.listen(8080);
+console.log('hello-world')
